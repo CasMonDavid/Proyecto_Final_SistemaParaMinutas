b17d0886d46cf4ca81b89956804b6e913395977d
CasMonDavid <dcastro_21@alu.uabcs.mx>
2024-12-05 21:28:21 -0700

    Back: móduficación en routes/api para el servicio de programación móvil

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,25 @@
 //use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsersController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProjectsController;
+use App\Http\Controllers\Users_ProjectsController;
 
-/*Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');*/
+//Controladores para front de laravel
+// AUTENTICACIÓN
+Route::get('/auth', [AuthController::class, 'getUser']);
+Route::post('/auth', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
 
-Route::post('/user', [UsersController::class, 'add']);
+// USUARIOS
+Route::resource('usuarios', UsersController::class)
+    ->parameters(['usuarios' => 'user_id'])
+    ->names('user');
+
+// PROYECTOS
+Route::resource('proyectos', ProjectsController::class)
+    ->parameters(['proyectos' => 'project_id'])
+    ->names('projects');
+Route::get('/user_project/getByProject/{project}', [Users_ProjectsController::class, 'getByProject']);
+Route::get('/user_project/getById/{id}', [Users_ProjectsController::class, 'show']);
+Route::get('/user_project', [Users_ProjectsController::class, 'index']);
